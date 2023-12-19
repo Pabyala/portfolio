07c78535506ba80bf86da050bea6e26f317270e4
Eleomar Fajutnao <109301348+Pabyala@users.noreply.github.com>
2023-12-19 11:18:04 +0800

    Update WhatCanIDo.jsx

--- src/Components/WhatCanIDo.jsx
+++ src/Components/WhatCanIDo.jsx
@@ -1,40 +1,19 @@
-import React, { useRef } from 'react'
+import React from 'react'
 import '../Pages/AboutStyle.css'
 import { Row, Col } from 'react-bootstrap';
 import { WhatCanIDoItem } from '../Data/MyData';
-import { useAnimation, useInView } from 'framer-motion';
-import { AboutAnimationWCID } from './AnimateComponent';
 
 export const WhatCanIDo = () => {
-    const mainControls = useAnimation();
-    const ref = useRef(null)
-    const isInview = useInView(ref, {once: true});
-  
-    if (isInview) {
-        mainControls.start("visible");
-    }
-
     return (
-        <Row className='position-relative w-full overflow-hidden py-1'>
+        <Row className='wcid-row position-relative w-full overflow-hidden'>
             {WhatCanIDoItem.map((item, index) => {
-                const delayIncrement = 0.3;
-                const individualDelay = index * delayIncrement;
                 return(
-                    <Col key={index} sm={4} className='col px-3'>
-                        <AboutAnimationWCID
-                            customStyle="col-card px-5 py-4 d-flex flex-column align-items-center justify-content-center"
-                            customTransition={{ 
-                                ease: 'easeInOut', 
-                                duration: 2, 
-                                delay: 1 + individualDelay
-                            }}
-                        >
-                            {/* <div className='col-card px-5 py-4 d-flex flex-column align-items-center justify-content-center'> */}
-                                <img className='svg mb-2' src={item.img} alt=""/>
-                                <h5 className='title-card text-center fs-5'>{item.titleCard}</h5>
-                                <p className='sub-card text-center mb-0 fs-6'>{item.textContent}</p>
-                            {/* </div> */}
-                        </AboutAnimationWCID>
+                    <Col key={index}  md={4} lg={4} className='wcid col'>
+                        <div className='wcid-wrap-card'>
+                            <img className='svg' src={item.img} alt=""/>
+                            <h5 className='title-card'>{item.titleCard}</h5>
+                            <p className='sub-card'>{item.textContent}</p>
+                        </div>
                     </Col>
                 )
             })}
