bd0a4313dd4f6035e3b28114cf27e16548f6be48
Eleomar Fajutnao <109301348+Pabyala@users.noreply.github.com>
2023-12-19 11:22:26 +0800

    Update Contact.jsx

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -3,30 +3,30 @@ import './ContactStyle.css'
 import ContactImg from '../Asset/Hands - Phone.png'
 import { ContactSkeleton, ContactSkeletonTitles } from '../Skeleton/SkeletonContact';
 import { ContactForm } from '../Components/ContactForm';
-import { ContactAnimation } from '../Components/AnimateComponent';
 
 export const Contact = () => {
   const [loading, setLoading] = useState(true);
   
   useEffect(() => {
-    setTimeout(() => {
+    const timeoutLoading = setTimeout(() => {
       setLoading(false);
     }, 2000);
+
+    return () => clearTimeout(timeoutLoading)
+    // eslint-disable-next-line
   }, []);
 
   return (
-    <section className='contact' id='contact'>
+    <section className='contact w-100' id='contact'>
       {loading ? (<ContactSkeletonTitles/>) 
       : (
-        <ContactAnimation>
-        <h1 className='contact-title fs-2 mb-3 position-relative'>Contacts</h1>
-        </ContactAnimation>
+        <h1 className='contact-title position-relative'>Contacts</h1>
       )}
-      
+
       {loading ? (<ContactSkeleton/>)
       : (
-        <div className='contact-row row col-12'>
-          <div className="contact-body col-6 d-flex align-items-center justify-content-center">
+        <div className='contact-row'>
+          <div className="contact-body d-flex align-items-center justify-content-center">
               <div className="img-con">
                   <img className='contact-img' src={ContactImg} alt="" />
               </div>
