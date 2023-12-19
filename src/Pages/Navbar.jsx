9606aa04103cd0dcff45211fb6d5585875753209
Eleomar Fajutnao <109301348+Pabyala@users.noreply.github.com>
2023-12-19 11:26:20 +0800

    Update Navbar.jsx

--- src/Pages/Navbar.jsx
+++ src/Pages/Navbar.jsx
@@ -1,34 +1,40 @@
 import React, { useState } from 'react'
 import { BiMenuAltRight } from "react-icons/bi";
 import './NavbarStyle.css'
+import { Container } from 'react-bootstrap';
 
 export const Navbar = () => {
+
     const [clickHumburger, setClickHumburger] = useState(false)
+    
     return (
         <div className='navbar-menu'>
-            <nav className="nav d-flex mx-auto align-items-center h-25">
-                <a className="navbar-brand me-auto fs-4 custom-fw" href="/">MarF.</a>
-                <div className="navbar-humburger" onClick={()=> setClickHumburger(!clickHumburger)}>
+            <Container className=''>
+            <nav className="nav d-flex align-items-center">
+                <a className="navbar-brand" href="/">MarF.</a>
+                <div className="navbar-humburger d-lg-none" onClick={()=> setClickHumburger(!clickHumburger)}>
                     <BiMenuAltRight className='humburgerIcon'/>
                 </div>
-                <ul className={`nav-ul ${ clickHumburger ? 'active' : ''} list-unstyled d-flex justify-content-center align-items-center m-0`}>
+                <ul className={`nav-ul ${ clickHumburger ? 'active' : ''}`}>
                     <li className="nav-item">
-                        <a className="nav-link custom-color" href='#home' >Home</a>
+                        <a className="nav-link" href='#home' >Home</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link custom-color" href='#about' >About</a>
+                        <a className="nav-link" href='#about' >About</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link custom-color" href='#skills' >Skill</a>
+                        <a className="nav-link" href='#skills' >Skill</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link custom-color" href='#project' >Project</a>
+                        <a className="nav-link" href='#project' >Project</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link custom-color contact-menu" href='#contact'>Contact</a>
+                        <a className="nav-link contact-menu" href='#contact'>Contact</a>
                     </li>
                 </ul>
             </nav>
+            </Container>
         </div>
+        
     )
 }
