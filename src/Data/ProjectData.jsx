388e1a7db521e34e0061fd3146cdf99aad95db7e
Eleomar Fajutnao <109301348+Pabyala@users.noreply.github.com>
2023-12-19 11:20:53 +0800

    Create ProjectData.jsx

---
+++ src/Data/ProjectData.jsx
@@ -0,0 +1,72 @@
+import OnePieaceProject from '../Asset/One Piece.png'
+import IDGeneratorProject from "../Asset/ID's Generator.png"
+import GShortcutProject from "../Asset/GShortcut.png"
+import MarFShoeStoreProject from "../Asset/Min-Shoe-Store.png"
+import WeatherAppProject from '../Asset/Weather-app.jpg'
+import MovieHubProject from "../Asset/movie.png"
+
+export const ProjectData = [
+    {
+        id: 1,
+        img: OnePieaceProject,
+        projectTitle: "One Piece",
+        projectDetails: 'Provide a list of pirate groups, as well as the ongoing story in the anime and' + 
+                        ' the birthday list of each character. It also has a short quiz with 25 items.',
+        projectTechUse: "HTML, CSS, ReactJs, Responsive Design",
+        projectLinkLive: "https://pabyala.github.io/onepiece/",
+        projectLinkCode: "https://github.com/Pabyala/onepiece"
+    },
+    {
+        id: 2,
+        img: IDGeneratorProject,
+        projectTitle: "ID's Generator",
+        projectDetails: 'This website generates 4 unique school IDs, and you can choose the school you want.' +
+                        ' All you need to do is fill up the form and download your ID.',
+        projectTechUse: "HTML, CSS, JavaScript, Responsive Design",
+        projectLinkLive: "https://pabyala.github.io/id-generator/",
+        projectLinkCode: "https://github.com/Pabyala/id-generator"
+    },
+    {
+        id: 3,
+        img: GShortcutProject,
+        projectTitle: "Google Shortcu",
+        projectDetails: 'This "G-Shortcut" a platform enabling you to create personalized shortcuts' + 
+                        ' effortlessly. Just enter the name and valid URL of your choice. Once added,' + 
+                        ' easily click on these shortcuts to access the webpages or sites.',
+        projectTechUse: "HTML, CSS, ReactJs, Bootstrap, Responsive Design",
+        projectLinkLive: "https://pabyala.github.io/shortcut/",
+        projectLinkCode: "https://github.com/Pabyala/shortcut"
+    },
+    {
+        id: 4,
+        img: MarFShoeStoreProject,
+        projectTitle: "MarF Shoe Store",
+        projectDetails: "It's an online shoe shop where you can choose your favorite sneakers," +
+                        " adding them to your cart. The shop offers four different brands of shoes:" +
+                        " Nike, Adidas, Vans, and Converse.",
+        projectTechUse: "HTML, CSS, Bootstrap, ReactJs, Responsive Design",
+        projectLinkLive: "https://pabyala.github.io/MarF.shoe-shop/",
+        projectLinkCode: "https://github.com/Pabyala/MarF.shoe-shop"
+    },
+    {
+        id: 5,
+        img: WeatherAppProject,
+        projectTitle: "Weather Forecast",
+        projectDetails: "It's a website that shows the current weather forecast and a five-day forecast." +
+                        " You can also search for the country or city that you want to see the weather for.",
+        projectTechUse: "HTML, CSS, Bootstrap, ReactJs, Responsive Design",
+        projectLinkLive: "https://pabyala.github.io/weatherforecast-app/",
+        projectLinkCode: "https://github.com/Pabyala/weatherforecast-app"
+    },
+    {
+        id: 6,
+        img: MovieHubProject,
+        projectTitle: "Movies Hub",
+        projectDetails: "It's a website where you can see popular movies and TV series. Choose a genre," +
+                        " and it will display matching movies or TV series. You can also use the search" +
+                        " function to find your favorites movie or TV series.",
+        projectTechUse: "HTML, CSS, Bootstrap, Material UI, ReactJs, Responsive Design",
+        projectLinkLive: "https://m0vieshub.netlify.app",
+        projectLinkCode: "https://github.com/Pabyala/movies.hub"
+    },
+]
