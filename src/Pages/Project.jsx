3ae1e684e36874365b02da74b4efb8da58cca8de
Eleomar Fajutnao <109301348+Pabyala@users.noreply.github.com>
2023-12-19 11:28:14 +0800

    Update Project.jsx

--- src/Pages/Project.jsx
+++ src/Pages/Project.jsx
@@ -1,52 +1,43 @@
 import React, { useEffect, useState } from 'react'
 import './ProjectStyle.css'
 import { ProjectSkeletonBtn, ProjectSkeletonTitles } from '../Skeleton/SkeletonProject';
-import { ProjectAnimation } from '../Components/AnimateComponent';
 import MyProjectContent from '../Components/MyProjectContent';
 
 export const Project = () => {
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-      setTimeout(() => {
+        const timeoutLoading = setTimeout(() => {
         setLoading(false);
       }, 2000);
+
+      return () => clearTimeout(timeoutLoading)
+      // eslint-disable-next-line
     }, []);
 
     return (
-        <section className='project' id='project'>
+        <section className='project' id='project w-100'>
             {loading ? (<ProjectSkeletonTitles/>)
             :(
                 <>
-                    <ProjectAnimation>
-                        <h1 className='prject-header-title fs-2 mb-3 position-relative'>My Projects</h1>
-                    </ProjectAnimation>
-                    <ProjectAnimation
-                        customTransition={{ 
-                            ease: 'easeInOut', 
-                            duration: 1, 
-                            delay: 0.4
-                        }}
-                    >
-                        <p className='project-text mb-0 fs-6'>Here are some of my personal projects.</p>
-                    </ProjectAnimation>
+                    <h1 className='prject-header-title position-relative'>My Projects</h1>
+                    <p className='project-text'>Here are some of my personal projects.</p>
                 </>
             )}
 
             <MyProjectContent/>
             
-            <div className='d-flex align-items-center justify-content-center mt-4'>
+            <div className='more-wrap d-flex align-items-center justify-content-center'>
                 {loading ? (<ProjectSkeletonBtn/>)
                 : (
-                    <ProjectAnimation>
-                    <a className='btn-show-more outline-none border-0 py-2 px-3 bg-black text-white fs-6 outline-none'
-                    target="_blank"
-                    rel="noreferrer"
-                    href="https://github.com/Pabyala"
+                    <a 
+                        className='btn-show-more outline-none bg-black text-white'
+                        target="_blank"
+                        rel="noreferrer"
+                        href="https://github.com/Pabyala"
                     >
                         Show more...
                     </a>
-                    </ProjectAnimation>
                 )}
             </div>
         </section>
