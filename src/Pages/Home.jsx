f38b6ed28be0f0c53655fe5d9d59e778265138c4
Eleomar Fajutnao <109301348+Pabyala@users.noreply.github.com>
2023-12-19 11:25:27 +0800

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,58 +1,31 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './HomeStyle.css'
 import { AiFillGithub } from "react-icons/ai";
 import { HomeSkeletonImg, HomeSkeletonInfo } from '../Skeleton/SkeletonHome';
-import { useAnimation, useInView } from 'framer-motion'
-import { HomeAnimationGitHubLink, HomeAnimationInfo, HomeAnimationName, 
-  HomeAnimationPositon, HomeAnimationProfile, HomeAnimationTitle 
-} from '../Components/AnimateComponent';
 
 export const Home = () => {
   const [loading, setLoading] = useState(true)
-  const ref = useRef(null)
-  const isInview = useInView(ref, {once: true});
-  const mainControls = useAnimation()
 
   useEffect(() => {
-    setTimeout(() => {
-      setLoading(false) 
-    }, 2000)
-  })
+    const timeoutLoading = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
 
-
-  useEffect(() => {
-      if(isInview){
-          mainControls.start("visible")
-      }
-  }, [isInview, mainControls])
-
-
+    return () => clearTimeout(timeoutLoading)
+    // eslint-disable-next-line
+  }, []);
 
   return (
-    <section id='home' className="home-content d-flex justify-content-between align-items-center mx-auto">
-      
+    <section id='home' className="home-content w-100">
       <div className="bg-dot"></div>
 
       {loading ? (<HomeSkeletonInfo/> ) :
-      (<div className="home-info d-flex flex-column justify-content-end w-50"> 
-        <HomeAnimationTitle>
-          <h5 ref={ref} className="home-subtitle position-relative fs-4 span-subtitle">Hi, I am</h5>
-        </HomeAnimationTitle>
-
-        <HomeAnimationName>
+      (<div className="home-info"> 
+          <h5 className="home-subtitle position-relative span-subtitle">Hi, I am</h5>
           <h1 className='home-name my-1'>Eleomar Fajutnao</h1>
-        </HomeAnimationName>
-
-        <HomeAnimationPositon>
           <h2 className='home-title mb-1'>Junior Front End Developer</h2>
-        </HomeAnimationPositon>
-
-        <HomeAnimationInfo>
-          <p className='home-sub-info fs-6 fst-italic'>I love exploring and building things for the web.</p>
-        </HomeAnimationInfo>
-
-        <HomeAnimationGitHubLink>
-          <a className='home-github text-decoration-none py-2 d-flex justify-content-center align-items-center' 
+          <p className='home-sub-info fst-italic'>I love exploring and building things for the web.</p>
+          <a className='home-github text-decoration-none d-flex justify-content-center align-items-center' 
             href="https://github.com/Pabyala"
             target="_blank"
             rel="noopener noreferrer"
@@ -60,15 +33,13 @@ export const Home = () => {
             Github 
             <AiFillGithub className='githubIcon'/>
           </a>
-        </HomeAnimationGitHubLink>
       </div>)}
       
       {loading ? (<HomeSkeletonImg/> ) :
       (<div className="profile-img-body d-flex justify-content-center align-items-center">
-        <HomeAnimationProfile>
           <div className="profile-img"></div>
-        </HomeAnimationProfile>
       </div>)}
+
     </section>
   )
 }
