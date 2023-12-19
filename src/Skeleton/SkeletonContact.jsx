486edb4ccfece7028e9436e60d42c226798b4237
Eleomar Fajutnao <109301348+Pabyala@users.noreply.github.com>
2023-12-19 11:30:37 +0800

    Update SkeletonContact.jsx

--- src/Skeleton/SkeletonContact.jsx
+++ src/Skeleton/SkeletonContact.jsx
@@ -4,12 +4,12 @@ import './SkeletonStyle.css'
 
 export const ContactSkeleton = () => {
     return (
-        <div className='skeleton-contact-row row col-12'>
-            <div className="skeleton-contact-body col-6">
+        <div className='skeleton-contact-row'>
+            <div className="skeleton-contact-body">
                 <div className="skeleton-contact-img">
                 </div>
             </div>
-            <form className='skeleton-contact-form col-6'>
+            <form className='skeleton-contact-form'>
                 <div className='skeleton-contact-subTitle'></div>
                 <div className="skeleton-contac-input contac-input"></div>
                 <div className="skeleton-contac-input contac-input"></div>
