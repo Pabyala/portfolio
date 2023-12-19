a13cb5ba1885173c6c77a8845c74dae43f39e5e9
Eleomar Fajutnao <109301348+Pabyala@users.noreply.github.com>
2023-12-19 11:28:57 +0800

    Update Skills.jsx

--- src/Pages/Skills.jsx
+++ src/Pages/Skills.jsx
@@ -2,47 +2,29 @@ import React, { useEffect, useState } from 'react'
 import './SkillsStyle.css'
 import { SkillsSkeleton, SkillsSkeletonTitles } from '../Skeleton/SkeletonSkills';
 import { TechSkill } from '../Components/TechSkill';
-import { SkillsAnimationTitle } from '../Components/AnimateComponent';
 
 export const Skills = () => {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    setTimeout(() => {
+    const timeoutLoading = setTimeout(() => {
       setLoading(false);
     }, 2000);
+
+    return () => clearTimeout(timeoutLoading)
+    // eslint-disable-next-line
   }, []);
 
   return (
-    <section className='skills' id='skills'>
+    <section className='skills w-100' id='skills'>
         {loading ? (<SkillsSkeletonTitles/>)
         :( 
           <>
-          <SkillsAnimationTitle>
-            <h1 className='skill-title fs-2 mb-3 position-relative'>Skills & Tools</h1>
-          </SkillsAnimationTitle>
-
-          <SkillsAnimationTitle
-            customTransition={{ 
-              ease: 'easeInOut', 
-              duration: 2, 
-              delay: 0.4
-            }}
-          >
-            <h2 className='skill-subtitle fw-bold fs-3 mb-0'>My toolbox</h2>
-          </SkillsAnimationTitle>
-
-          <SkillsAnimationTitle
-            customTransition={{ 
-              ease: 'easeInOut', 
-              duration: 2, 
-              delay: 0.5
-            }}
-          >
-            <p className='skill-text fs-6'>
+            <h1 className='skill-title position-relative'>Skills & Tools</h1>
+            <h2 className='skill-subtitle mb-0'>My toolbox</h2>
+            <p className='skill-text'>
               The skills, tools and technologies I use to develop the website
             </p>
-          </SkillsAnimationTitle>  
           </> 
         )}
 
